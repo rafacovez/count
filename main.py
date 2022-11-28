ec3e315409181ee087115e020b762b7194713b6c
Rafa ✏️ <adanescollante@gmail.com>
2022-11-28 18:30:06 -0500

    added negative number rule for value input to count to

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Declare variable
+# Declare variables
 
 x = 0
 number = 0
@@ -8,8 +8,9 @@ number = 0
 def inputValue():
   global number
   number = int(input("What number do you want me to count to? "))
-  if number < 0:
-    int(input("I can't count to a negative number! let's try again... "))
+  while number < 0:
+    number = int(input("I can't count to a negative number! let's try again... "))
+    continue
 
 # Count to number given
 
