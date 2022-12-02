406864ff3ce532ca2a85a43314f9635b15d64b34
Rafa ✏️ <adanescollante@gmail.com>
2022-12-02 16:52:09 -0500

    added try and except functions to handle error in case input is not a positive number

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-# Declare variables
 x = 0
 number = 0
 
 # Ask for input
 def inputValue():
   global number
-  number = input("How many sheeps do you want me to count? ")
-  isNumeric = number.isnumeric()
-  while not isNumeric:
-    number = input("I can't count to a negative number of sheeps! let's try again... ")
-    isNumeric = number.isnumeric()
-  number = int(number)
 
-# Count to number given
+  number = input("How many sheeps do you want me to count? ")
+
+  # Handle error in case input is not a positive number
+  try:
+    number = int(number)
+  except:
+    number = -1
+
 def count():
   global x
-  while x < number:
-    x = x + 1
-    print(x, "sheeps")
+  
+  # Check if variable number is a positive number
+  if number < 0:
+    print("That's not a valid number")
+  # Count to number given
+  else:
+    while x < number:
+      x = x + 1
+      print(x, "sheeps")
 
 def main():
   inputValue()
