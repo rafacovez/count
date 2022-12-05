486a1c576b6220b541d9e58d4d5ed00b32ec06a9
Rafa ✏️ <adanescollante@gmail.com>
2022-12-05 08:00:07 -0500

    reduced code removing functions

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
+# get input from user
+number = input("How many sheeps do you want me to count? ")
+
+# handle error in case input is not a positive number
+try:
+  number = int(number)
+except:
+  number = -1
+
 x = 0
-number = 0
 
-# Ask for input
-def inputValue():
-  global number
-
-  number = input("How many sheeps do you want me to count? ")
-
-  # Handle error in case input is not a positive number
-  try:
-    number = int(number)
-  except:
-    number = -1
-
-def count():
-  global x
-  
-  # Check if variable number is a positive number
-  if number < 0:
-    print("That's not a valid number")
-  # Count to number given
-  else:
-    while x < number:
-      x = x + 1
-      print(x, "sheeps")
-
-def main():
-  inputValue()
-  count()
-
-main()
+# count sheep to number given
+if number < 0:
+  print("That's not a valid input")
+else:
+  while x < number:
+    x += 1
+    print(x, "sheeps")
